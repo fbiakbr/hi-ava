1612b153adb86dc12257f265d6939dcf8341a6da
Febiadi Wisnu Akbar <63489969+fbiakbr@users.noreply.github.com>
2020-05-05 01:24:55 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "fbiakbr", quotes: "Whatever happens, keep breathing" },
+    { author: "fbiakbr", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "fbiakbr", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "fbiakbr", quotes: "I love you." },
+    { author: "fbiakbr", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "fbiakbr", quotes: "I love you." },
+    { author: "fbiakbr", quotes: "I'm sorry for loving you." },
 ];
