34d3ce8c4d8829c4ad8c4e2d85ffaa628fd9ec05
Febiadi Wisnu Akbar <63489969+fbiakbr@users.noreply.github.com>
2020-05-05 01:35:56 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,8 +1,7 @@
 // prettier-ignore
 export default [
     { author: "fbiakbr", quotes: "Whatever happens, keep breathing" },
-    { author: "fbiakbr", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "fbiakbr", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "fbiakbr", quotes: "Hey! I miss you! UwU" },
     { author: "fbiakbr", quotes: "I love you." },
     { author: "fbiakbr", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "fbiakbr", quotes: "I love you." },
